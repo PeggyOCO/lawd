ae66986a9808973c1499dda1a7a02116ebab634e
PeggyOCO <peggy@mariposagraphics.com>
2015-11-13 11:36:29 -0800

    vars for largeright

--- sites/all/themes/city_of_angels/template.php
+++ sites/all/themes/city_of_angels/template.php
@@ -5,3 +5,8 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * City of Angels theme.
  */
+function city_of_angels_preprocess_field(&$variables) {
+	if($variables['element']['#field_name'] == 'field_largeright') {
+            dsm($variables);
+	}
+}
