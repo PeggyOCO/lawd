c800366a959c7b76af35888d27673e55d9de2357
PeggyOCO <peggy@mariposagraphics.com>
2015-11-13 01:26:08 -0800

    vars

--- sites/all/themes/city_of_angels/template.php
+++ sites/all/themes/city_of_angels/template.php
@@ -7,5 +7,10 @@
  */
 function city_of_angels_preprocess_node(&$variables) {
 
+  $wrapper = entity_metadata_wrapper('node', $node); 
+  foreach ($wrapper->field_largeright as $i){
+      //print $i->field_image1->value(); 
+      dsm($i);
+ }
 
 }
