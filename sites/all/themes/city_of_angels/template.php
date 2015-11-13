4c4a472d676d1c414c5216a72f46888d6139a753
PeggyOCO <peggy@mariposagraphics.com>
2015-11-13 01:02:17 -0800

    templat php

--- sites/all/themes/city_of_angels/template.php
+++ sites/all/themes/city_of_angels/template.php
@@ -4,8 +4,8 @@
  * @file
  * Template overrides as well as (pre-)process and alter hooks for the
  * City of Angels theme.
-<<<<<<< HEAD
  */
-=======
- */
->>>>>>> 19d3ffbe9d36ef42abd2423b11a48e2501aee348
+function city_of_angels_preprocess_node(&$variables) {
+dsm($variables);
+
+}
