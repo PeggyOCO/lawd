82f5cc887cadf35267ac545cb33c300736be05df
PeggyOCO <peggy@mariposagraphics.com>
2015-11-13 01:30:08 -0800

    vars

--- sites/all/themes/city_of_angels/template.php
+++ sites/all/themes/city_of_angels/template.php
@@ -5,12 +5,3 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * City of Angels theme.
  */
-function city_of_angels_preprocess_page(&$variables) {
-
-  $wrapper = entity_metadata_wrapper('node', $node); 
-  foreach ($wrapper->field_largeright as $i){
-      //print $i->field_image1->value(); 
-      dsm($i);
- }
-
-}
