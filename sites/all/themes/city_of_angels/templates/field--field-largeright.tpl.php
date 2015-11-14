238be439c93f0e4c11d611f8e78280e9f70025e9
PeggyOCO <peggy@mariposagraphics.com>
2015-11-13 20:43:16 -0800

    styling custom template

--- sites/all/themes/city_of_angels/templates/field--field-largeright.tpl.php
+++ sites/all/themes/city_of_angels/templates/field--field-largeright.tpl.php
@@ -28,5 +28,5 @@ $field_type_css: The css-compatible field type.
 <div style="border:1px solid red;"><?php print render($items[0]);?></div>
 
 <div style="border:1px solid orange;"><?php print render($items[2]);?></div>
-
+<div style="border:1px solid blue;"><?php print render($items[3]);?></div>
 
