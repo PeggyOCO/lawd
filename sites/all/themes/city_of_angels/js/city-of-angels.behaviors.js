e4317a928670a3819c4fe829a077b6a654e0bff0
PeggyOCO <peggy@mariposagraphics.com>
2015-11-25 00:28:19 -0800

    caption css

--- sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
+++ sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
@@ -62,7 +62,7 @@
 	$('.field-name-field-fourup a img').jcaption({
 	  wrapperElement: 'a',
 		copyStyle: true,
-		animate: false,
+		animate: true,
 		show: {height: "show"},
 		hide: {height: "hide"}
 	});
@@ -70,7 +70,7 @@
 		$('.field-name-field-rightbig a img').jcaption({
 		wrapperElement: 'a',
 		copyStyle: true,
-		animate: false,
+		animate: true,
 		show: {height: "show"},
 		hide: {height: "hide"}
 	});
