e7a9a617a8fa09a5acefc781f27355168642535c
PeggyOCO <peggy@mariposagraphics.com>
2015-11-26 12:15:53 -0800

    caption css

--- sites/all/themes/city_of_angels/template.php
+++ sites/all/themes/city_of_angels/template.php
@@ -7,7 +7,7 @@
  */
 function city_of_angels_preprocess_field(&$variables) {
 	if($variables['element']['#field_name'] == 'field_fourup') {
-            dsm($variables['items'][0]['#item']);
+            //dsm($variables['items'][0]['#item']);
             $variables['items'][0]['#prefix']="<div class='inline'>";
             $variables['items'][0]['#suffix']="</div>";
 	}
