df883918d663b1bed5d1305ff4d8be89e926f242
PeggyOCO <peggy@mariposagraphics.com>
2015-11-29 09:57:16 -0800

    added jquery fix for dynamic image resize

--- sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
+++ sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
@@ -77,6 +77,44 @@
 
 $('.inlinefourup a').prop("href", "/dev/node/24");
 
+
+$('.field-name-field-fourup .inlinefourup a img').each(function(i, item) {
+    var img_height = $(item).height();
+    var div_height = $(item).parent().height();
+    if(img_height<div_height){
+        //IMAGE IS SHORTER THAN CONTAINER HEIGHT - CENTER IT VERTICALLY
+        var newMargin = (div_height-img_height)/2+'px';
+        $(item).css({'margin-top': newMargin });
+    }else if(img_height>div_height){
+        //IMAGE IS GREATER THAN CONTAINER HEIGHT - REDUCE HEIGHT TO CONTAINER MAX - SET WIDTH TO AUTO  
+        $(item).css({'width': 'auto', 'height': '100%'});
+        //CENTER IT HORIZONTALLY
+        var img_width = $(item).width();
+        var div_width = $(item).parent().width();
+        var newMargin = (div_width-img_width)/2+'px';
+        $(item).css({'margin-left': newMargin});
+    }
+});
+
+$('.field-name-field-rightbig a img').each(function(i, item) {
+    var img_height = $(item).height();
+    var div_height = $(item).parent().height();
+    if(img_height<div_height){
+        //IMAGE IS SHORTER THAN CONTAINER HEIGHT - CENTER IT VERTICALLY
+        var newMargin = (div_height-img_height)/2+'px';
+        $(item).css({'margin-top': newMargin });
+    }else if(img_height>div_height){
+        //IMAGE IS GREATER THAN CONTAINER HEIGHT - REDUCE HEIGHT TO CONTAINER MAX - SET WIDTH TO AUTO  
+        $(item).css({'width': 'auto', 'height': '100%'});
+        //CENTER IT HORIZONTALLY
+        var img_width = $(item).width();
+        var div_width = $(item).parent().width();
+        var newMargin = (div_width-img_width)/2+'px';
+        $(item).css({'margin-left': newMargin});
+    }
+});
+
+
 });
 
 })(jQuery);
