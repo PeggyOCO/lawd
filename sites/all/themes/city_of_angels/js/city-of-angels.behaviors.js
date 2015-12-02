282dfd9583fbff17d6237532956b8da03e130f50
PeggyOCO <peggy@mariposagraphics.com>
2015-12-01 23:27:30 -0800

    mods and tests for behaviors

--- sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
+++ sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
@@ -60,7 +60,7 @@
   
   $(document).ready(function(){
   alert('got here');
-drupal_add_js('/dev/sites/all/themes/city_of_angels/js/jcaption.min.js', file);
+drupal_add_js('themes/city_of_angels/js/jcaption.min.js');
      /*   alert($(window).width() + ' got here' + $base_url);*/
     
     
