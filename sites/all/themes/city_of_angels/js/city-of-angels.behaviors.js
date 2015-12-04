5b27ae218e0402e400a07748a6dcb18caa2acb3c
PeggyOCO <peggy@mariposagraphics.com>
2015-12-04 10:59:11 -0800

    mods to links

--- sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
+++ sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
@@ -88,20 +88,22 @@ $('.field-name-field-rightbig a').prop("href", realurl);*/
 
 $('.field-name-field-rightbig').each( function( index, element ){
     var realurlbig = $(this).children("a").children(".caption").children("img").attr('title');
-     $(this).children("a").data("href","/dev/los-angeles-web-design/"+realurlbig+"");
+     $(this).children("a").prop("href","/dev/los-angeles-web-design/"+realurlbig+"");
   /* $(this).children("a").data("href",'/dev/los-angeles-web-design/' + realurlbig);*/
    /* $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurlbig+'');*/
 });
 
 $('.inlinefourup').each( function( index, element ){
     var realurltiny = $(this).children("a").children(".caption").children("img").attr('title');
-  $(this).children("a").data("href","/dev/los-angeles-web-design/"+realurltiny+"");
+  $(this).children("a").prop("href","/dev/los-angeles-web-design/"+realurltiny+"");
     /* $(this).children("a").data("href",'/dev/los-angeles-web-design/' + realurltiny);*/
    /* $(this).children("a").attr("href",'/dev/los-angeles-web-design/' + realurltiny);*/
      /* $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurltiny+ '');*/
 });
 
-
+$('a').click(function() {
+  alert($(this).data('href') || $(this).attr('href'));
+});
 
 $('.field-name-field-fourup .inlinefourup a .caption img').each(function(i, item) {
     var img_height = $(item).height();
