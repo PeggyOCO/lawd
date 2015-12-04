f44a72bb4682ccd614d17831a60e2e78f2d9cf50
PeggyOCO <peggy@mariposagraphics.com>
2015-12-04 10:42:30 -0800

    mods to links

--- sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
+++ sites/all/themes/city_of_angels/js/city-of-angels.behaviors.js
@@ -90,7 +90,7 @@ $('.field-name-field-rightbig').each( function( index, element ){
     var realurlbig = $(this).children("a").children(".caption").children("img").attr('title');
      $(this).children("a").prop("href","/dev/los-angeles-web-design/"+realurlbig+"");
   /* $(this).children("a").data("href",'/dev/los-angeles-web-design/' + realurlbig);*/
-    $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurlbig+'');
+   /* $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurlbig+'');*/
 });
 
 $('.inlinefourup').each( function( index, element ){
@@ -98,7 +98,7 @@ $('.inlinefourup').each( function( index, element ){
   $(this).children("a").prop("href","/dev/los-angeles-web-design/"+realurltiny+"");
     /* $(this).children("a").data("href",'/dev/los-angeles-web-design/' + realurltiny);*/
    /* $(this).children("a").attr("href",'/dev/los-angeles-web-design/' + realurltiny);*/
-     $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurltiny+ '');
+     /* $(this).children("a").attr("onclick",'window.location=/dev/los-angeles-web-design/' + realurltiny+ '');*/
 });
 
 
